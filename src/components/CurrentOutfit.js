f9ca8216f9f29a300df048a966da06b92834dddc
Nathan Zeager <nathanzeager@hotmail.com>
2022-10-16 17:28:31 -0400

    Updated display of outfit items, updated display of outfit based on location, added information to the current outfit page

--- src/components/CurrentOutfit.js
+++ src/components/CurrentOutfit.js
@@ -1,5 +1,3 @@
-//TODO: Replace faulty image src with error image
-
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { SaveButton } from "./SaveButton";
@@ -9,9 +7,9 @@ export const CurrentOutfit = ({ currOutfit }) => {
     return(
         <>
             <h1>Current Outfit</h1>
-            <p>You haven't starting building an outfit yet. <a href='/'>Go to your closet to start adding items to your outfit.</a></p>
+            <p>{currOutfit.length === 0 ? "You haven't starting building an outfit yet." : `You have ${currOutfit.closet_item.length} closet items in your outfit so far.`} <a href='/'>Go to your closet to add items to your outfit.</a></p>
             <div> Outfit image goes here 
-                <DisplayOutfit outfit={currOutfit} />
+                <DisplayOutfit outfit={currOutfit} location='editOutfit' />
             </div>
             <IconButton color="primary" aria-label="clear outfit">
                 <DeleteIcon />
--- src/components/DisplayOutfit.js
+++ src/components/DisplayOutfit.js
@@ -1,9 +1,59 @@
-export const DisplayOutfit = ({ outfit }) => {
+//TODO: Replace faulty image src with error image
+
+import { DownhillSkiing } from "@mui/icons-material";
+
+export const DisplayOutfit = ({ outfit, location }) => {
+    // location determines how outfit displays depending on page it is used in. Options: 'myOutfits', 'editOutfit', 'viewOutfit'
+
+    // changes image width based on location
+    const size = location==='myOutfits' ? '50rem' : '100rem';
+
+    // arrays to collect the items of different types
+    const tops = [];
+    const outers = [];
+    const bottoms = [];
+    const shoes = [];
+
+    // subcategories for each type
+    const topTypes = ['button-down', 'dress', 'shirt', 'sweater', 't-shirt'];
+    const outerTypes = ['cardigan', 'coat', 'jacket', 'vest'];
+    const bottomTypes = ['pants', 'shorts', 'skirt'];
+    const shoeTypes = ['boots', 'flats', 'heels', 'sandals', 'slippers', 'sneakers'];
+
+    for (const item of outfit.closet_item) {
+        if (topTypes.includes(item.item_choice)) {
+            tops.push(item)
+        } else if (outerTypes.includes(item.item_choice)) {
+            outers.push(item)
+        } else if (bottomTypes.includes(item.item_choice)) {
+            bottoms.push(item)
+        } else if (shoeTypes.includes(item.item_choice)) {
+            shoes.push(item)
+        }        
+    }
+
     return (
         <>
-            {outfit.closet_item.map((item) => (
-                <div className={item.id}>{item.item_image ? <img src={item.item_image} alt='' width='100rem' /> : ''}</div>
-            ))}
+            <div className='tops'>
+                {tops.map((top) => (
+                    <div key={top.id}>{top.item_image ? <img src={top.item_image} alt='' width={size} /> : ''}</div>
+                ))}
+            </div>
+            <div className='outers'>
+                {outers.map((outer) => (
+                    <div key={outer.id}>{outer.item_image ? <img src={outer.item_image} alt='' width={size} /> : ''}</div>
+                ))}
+            </div>
+            <div className='bottoms'>
+                {bottoms.map((bottom) => (
+                    <div key={bottom.id}>{bottom.item_image ? <img src={bottom.item_image} alt='' width={size} /> : ''}</div>
+                ))}
+            </div>
+            <div className='shoes'>
+                {shoes.map((shoe) => (
+                    <div key={shoe.id}>{shoe.item_image ? <img src={shoe.item_image} alt='' width={size} /> : ''}</div>
+                ))}
+            </div>
         </>
     )
 }
